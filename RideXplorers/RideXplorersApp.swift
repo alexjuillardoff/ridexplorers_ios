beedfd5d7b18de17fac917314565e2a05c68af21
Alexis JUILLARD <alexjuillard@example.com>
2025-08-24 10:07:01 +0200

    Initial Commit

---
+++ RideXplorers/RideXplorersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideXplorersApp.swift
+//  RideXplorers
+//
+//  Created by Alexis JUILLARD on 24/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct RideXplorersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RideXplorersTests/RideXplorersTests.swift
@@ -0,0 +1,17 @@
+//
+//  RideXplorersTests.swift
+//  RideXplorersTests
+//
+//  Created by Alexis JUILLARD on 24/08/2025.
+//
+
+import Testing
+@testable import RideXplorers
+
+struct RideXplorersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
